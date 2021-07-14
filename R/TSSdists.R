7c87b52ef069c3c135ccb11b597829027c3bb7a1
Kristyna Kupkova <kristynakupkova@gmail.com>
2021-07-13 20:48:30 -0400

    make function for annotating yeast

---
+++ R/TSSdists.R
@@ -0,0 +1,81 @@
+#' Find the distance to the nearest genomic feature
+#'
+#' For a given query set of genomic regions, and a given feature set of
+#' regions, this function will return the distance for each query region to its
+#' closest feature. It ignores strand and returns the distance as positive or
+#' negative, depending on whether the feature is upstream or downstream
+#'
+#' This function is similar to the bioconductor distanceToNearest function, but
+#' returns negative values for downstream distances instead of absolute values.
+#' This allows you to assess the relative location.
+#'
+#' @param query A data.frame with following 3 columns: chr, start, end
+#' @param features A data.frame with gene coordinates
+#'
+#' @return A vector of genomic distances for each query region relative to its
+#'     closest feature.
+#' @export
+#' @examples
+#' vistaSftd = GenomicRanges::shift(vistaEnhancers, 100000)
+#' calcFeatureDist(vistaEnhancers, vistaSftd)
+calcFeatureDist_aY = function(query, features) {
+  if (is(query, "GRangesList")) {
+    # Recurse over each GRanges object
+    x = lapply(query, calcFeatureDist, features)
+    return(x)
+  }
+
+  # # splid the tables by chromosomes and make sure that mitochondrial
+  # # chromosome is called the same
+  # if ("chrmt" %in% query[, "chr"]){
+  #   query[query[,"chr"]=="chrmt","chr"] = "chrM"
+  # }
+  #
+  # if ("chrmt" %in% features[, "chr"]){
+  #   features[features[,"chr"]=="chrmt","chr"] = "chrM"
+  # }
+
+  query = as.data.table(query)
+  features = as.data.table(features)
+
+  queryDTs = splitDataTable_aY(query, split_factor="chr")
+  featureDTs = splitDataTable_aY(features, split_factor="chr")
+
+  annotatedPeaks = mapply(queryDTs, featureDTs[names(queryDTs)],
+                          FUN=DTNearest_aY)
+  finaltable = do.call("rbind", annotatedPeaks)
+}
+
+#' Function uses data.table rolling join to identify the nearest features
+#' really quickly.
+#'
+#' @param DT1 A data.table object to be joined to a second data.table object.
+#' @param DT2 A second data.table object to join with DT1.
+#'
+#' @return A rolling joined data.table object.
+#' @export
+DTNearest_aY = function(DT1, DT2) {
+  if (is.null(DT1)) {
+    return(NULL)
+  }
+  if (is.null(DT2)) {
+    return(rep(NA, nrow(DT1)))
+  }
+  # get middle point in
+  DT1[, mid:=start + round((end-start)/2)]
+  # mid here is TSS: on + strand start, on - strand end
+  DT2[, mid:=ifelse(strand == "+", start, end)]
+  data.table::setorder(DT1, mid)
+  data.table::setorder(DT2, mid)
+  data.table::setattr(DT1, "sorted", "mid")
+  data.table::setattr(DT2, "sorted", "mid")
+  data.table::setkey(DT1, mid)
+  data.table::setkey(DT2, mid)
+  merged = DT2[DT1, roll="nearest"]
+  merged[, distance:=ifelse(strand == "+", (mid-start), (end-mid))]
+
+  merged = as.data.frame(merged)
+  colnames(merged) = c("chrGene", "startGene", "endGene", "gene", "geneNum", "strand",
+                       "peakCenter", "chr", "start", "end", "distTSStoPeakCenter")
+  return(merged)
+}
---
+++ R/utility.R
@@ -0,0 +1,48 @@
+#' Checks class of the list of variables. To be used in functions
+#'
+#' @param checkList list of object to check, e.g.
+#' list(varname=c("data.frame", "numeric")).
+#' Multiuple strings in the vector are treated as OR.
+#' @return A warning if the wrong input class is provided.
+#' @examples
+#' x <- function(var1) {
+#'     cl = list(var1=c("numeric","character"))
+#'     .validateInputs(cl)
+#'     return(var1^2)
+#' }
+.validateInputs_aY <- function(checkList) {
+  nms = names(checkList)
+  for(i in seq_along(checkList)){
+    fail = FALSE
+    clss = checkList[[i]]
+    x = get(nms[i], envir=parent.frame(1))
+    for(cls in clss){
+      if (is(x, cls)) fail = append(fail, TRUE)
+    }
+    if(!any(fail))
+      stop(paste0(nms[i], " must be a ", paste(clss, collapse=" or "),
+                  ".  Got: ", class(x)))
+  }
+}
+
+#' Efficiently split a data.table by a column in the table
+#'
+#' @param DT Data.table to split
+#' @param split_factor Column to split, which can be a character vector
+#'        or an integer.
+#' @return List of data.table objects, split by column
+#' @export
+# @examples
+# DT = data.table::data.table(letters, grp = rep(c("group1", "group2"), 13))
+# splitDataTable(DT, "grp")
+# splitDataTable(DT, 2)
+splitDataTable_aY = function(DT, split_factor) {
+  factor_order = unique(DT[, get(split_factor)])
+  if (is.numeric(split_factor)) {
+    split_factor = colnames(DT)[split_factor]
+    message("Integer split_factor, changed to: ", split_factor)
+  }
+  l = lapply(split(seq_len(nrow(DT)), DT[, get(split_factor)]),
+             function(x) DT[x])
+  return(l[factor_order])
+}
